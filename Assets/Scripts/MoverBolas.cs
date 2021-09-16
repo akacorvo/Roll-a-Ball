30bdf3ff7b1afb1ead08794d8eec866da8da06ad
Carlos Benedito Tabares <al394807@uji.es>
2021-09-16 16:42:07 +0200

    No va pero bueno

--- Assets/Scripts/MoverBolas.cs
+++ Assets/Scripts/MoverBolas.cs
@@ -6,6 +6,8 @@ using UnityEngine.InputSystem;
 public class MoverBolas : MonoBehaviour
 {
     private Rigidbody rb;
+    private float movementX;
+    private float movementY;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +18,17 @@ public class MoverBolas : MonoBehaviour
     private void OnAMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+
     }
 
     void FixedUpdate()
     {
-        
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement);
     }
 }
   
